1d4d6741ea66c4918d41e6bb2624cd4109eacfec
Mieszko Gulinski <mieszko_gulinski@radcode.co>
2026-01-28 16:49:42 +0100

    add code to create certificate signing request (Csr)

---
+++ api/ksef_certificate.go
@@ -0,0 +1,126 @@
+package api
+
+import (
+	"context"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"encoding/base64"
+	"fmt"
+)
+
+// CertificateEnrollmentData stores the subject data required to prepare a certificate request.
+type CertificateEnrollmentData struct {
+	CommonName             string `json:"commonName"`
+	Surname                string `json:"surname,omitempty"`
+	SerialNumber           string `json:"serialNumber,omitempty"`
+	CountryName            string `json:"countryName"`
+	OrganizationName       string `json:"organizationName,omitempty"`
+	GivenName              string `json:"givenName,omitempty"`
+	UniqueIdentifier       string `json:"uniqueIdentifier"`
+	OrganizationIdentifier string `json:"organizationIdentifier,omitempty"`
+}
+
+var (
+	oidSurname                = asn1.ObjectIdentifier{2, 5, 4, 4}
+	oidGivenName              = asn1.ObjectIdentifier{2, 5, 4, 42}
+	oidUniqueIdentifier       = asn1.ObjectIdentifier{2, 5, 4, 45}
+	oidOrganizationIdentifier = asn1.ObjectIdentifier{2, 5, 4, 97}
+)
+
+// GetCertificateEnrollmentData returns the identification data used when building a new CSR.
+func (c *Client) GetCertificateEnrollmentData(ctx context.Context) (*CertificateEnrollmentData, error) {
+	token, err := c.getAccessToken(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	response := &CertificateEnrollmentData{}
+	resp, err := c.client.R().
+		SetContext(ctx).
+		SetAuthToken(token).
+		SetResult(response).
+		Get(c.url + "/certificates/enrollments/data")
+	if err != nil {
+		return nil, err
+	}
+	if resp.IsError() {
+		return nil, newErrorResponse(resp)
+	}
+
+	return response, nil
+}
+
+// GenerateCSR builds a PKCS#10 certificate signing request encoded in Base64.
+// The private key must be RSA and match the public key that will be embedded in the CSR.
+func (d *CertificateEnrollmentData) GenerateCSR(privateKey *rsa.PrivateKey) (string, error) {
+	if d == nil {
+		return "", fmt.Errorf("certificate enrollment data is nil")
+	}
+	if privateKey == nil {
+		return "", fmt.Errorf("private key is nil")
+	}
+	if err := privateKey.Validate(); err != nil {
+		return "", fmt.Errorf("invalid private key: %w", err)
+	}
+
+	subject, err := d.asPkixName()
+	if err != nil {
+		return "", err
+	}
+
+	template := &x509.CertificateRequest{
+		Subject:            subject,
+		SignatureAlgorithm: x509.SHA256WithRSA,
+	}
+
+	csr, err := x509.CreateCertificateRequest(rand.Reader, template, privateKey)
+	if err != nil {
+		return "", fmt.Errorf("create certificate request: %w", err)
+	}
+
+	return base64.StdEncoding.EncodeToString(csr), nil
+}
+
+func (d *CertificateEnrollmentData) asPkixName() (pkix.Name, error) {
+	if d.CommonName == "" {
+		return pkix.Name{}, fmt.Errorf("commonName is empty")
+	}
+	if d.CountryName == "" {
+		return pkix.Name{}, fmt.Errorf("countryName is empty")
+	}
+	if d.UniqueIdentifier == "" {
+		return pkix.Name{}, fmt.Errorf("uniqueIdentifier is empty")
+	}
+
+	name := pkix.Name{
+		CommonName:   d.CommonName,
+		SerialNumber: d.SerialNumber,
+	}
+
+	if d.CountryName != "" {
+		name.Country = []string{d.CountryName}
+	}
+	if d.OrganizationName != "" {
+		name.Organization = []string{d.OrganizationName}
+	}
+
+	name.ExtraNames = appendAttribute(name.ExtraNames, oidSurname, d.Surname)
+	name.ExtraNames = appendAttribute(name.ExtraNames, oidGivenName, d.GivenName)
+	name.ExtraNames = appendAttribute(name.ExtraNames, oidUniqueIdentifier, d.UniqueIdentifier)
+	name.ExtraNames = appendAttribute(name.ExtraNames, oidOrganizationIdentifier, d.OrganizationIdentifier)
+
+	return name, nil
+}
+
+func appendAttribute(attrs []pkix.AttributeTypeAndValue, oid asn1.ObjectIdentifier, value string) []pkix.AttributeTypeAndValue {
+	if value == "" {
+		return attrs
+	}
+	return append(attrs, pkix.AttributeTypeAndValue{
+		Type:  oid,
+		Value: value,
+	})
+}
